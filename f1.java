830bd347f54a59cc85440f105ad453162d833407
HardikG2002 <HardikG2002>
2022-09-13 18:14:53 +0530

    Added hello file

---
+++ f1.java
@@ -0,0 +1,8 @@
+public class f1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println(a+b);
+        System.out.println("sum");
+    }
+}
---
+++ h2.java
@@ -0,0 +1,7 @@
+import org.jcp.xml.dsig.internal.SignerOutputStream;
+
+public class h2 {
+    public static void main(String[] args) {
+        System.out.println("Hello2");
+    }
+}
